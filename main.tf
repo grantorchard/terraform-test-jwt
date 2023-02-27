45812fa66d051af4b998848dba1864b3a09e5fea
Grant Orchard <go@hashicorp.com>
2023-02-27 12:36:47 +1100

    add secret to retrieve

--- main.tf
+++ main.tf
@@ -1,3 +1,3 @@
 data "vault_generic_secret" "this" {
-    path = "secrets/data/prj-bdEfhBpNrpCrHbJn/ws-aKFaHhSGYhw8Y4T9/"
+    path = "secrets/data/prj-bdEfhBpNrpCrHbJn/ws-aKFaHhSGYhw8Y4T9["mypassword"]"
 }
